4b4316244ba02870398f573d405e0b752b24538a
BoyoungYun <index_1234@naver.com>
2023-10-25 15:29:55 +0900

    feat: Set current in steps component

--- Inha-graduate-FE/src/components/StepsComponent/StepsComponent.tsx
+++ Inha-graduate-FE/src/components/StepsComponent/StepsComponent.tsx
@@ -1,19 +1,13 @@
-import React, { useState } from "react";
 import { Steps } from "antd";
 
-const StepsComponent: React.FC = () => {
-  const [current, setCurrent] = useState(0);
-
-  const onChange = (value: number) => {
-    console.log("onChange:", value);
-    setCurrent(value);
-  };
-
+interface StepsComponentProps {
+  current: number;
+}
+export default function StepsComponent({ current }: StepsComponentProps) {
   return (
     <>
       <Steps
         current={current}
-        onChange={onChange}
         items={[
           {
             title: "어디로 갈까?",
@@ -28,6 +22,4 @@ const StepsComponent: React.FC = () => {
       />
     </>
   );
-};
-
-export default StepsComponent;
+}
