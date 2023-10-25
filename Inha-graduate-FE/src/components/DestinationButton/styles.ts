fc13cb1662b966f4984b3703773052ec5dae3f69
BoyoungYun <index_1234@naver.com>
2023-10-25 17:15:30 +0900

    feat: Render addition destination in destination picker

--- Inha-graduate-FE/src/components/DestinationButton/styles.ts
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const Block = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    gap: 10px;
-`;
--- Inha-graduate-FE/src/components/DestinationPicker/DestinationPicker.tsx
+++ Inha-graduate-FE/src/components/DestinationPicker/DestinationPicker.tsx
@@ -1,7 +1,9 @@
-import { DestinationButton } from "../DestinationButton";
-import { NoMarginTitle } from "./styles";
+import { useState } from "react";
+import { NoMarginTitle, Block, StyledButton } from "./styles";
+import { DownOutlined } from "@ant-design/icons";
 
 export default function DestinationPicker() {
+  const [buttonIdx, setButtonIdx] = useState(0);
   const items = [
     "서울",
     "부산",
@@ -11,6 +13,7 @@ export default function DestinationPicker() {
     "대전",
     "울산",
     "세종",
+    "제주",
     "경기",
     "강원",
     "충북",
@@ -19,12 +22,203 @@ export default function DestinationPicker() {
     "전남",
     "경북",
     "경남",
-    "제주",
+  ];
+  const optionalItems = [
+    [
+      "가평",
+      "동두천",
+      "부천",
+      "성남",
+      "수원",
+      "시흥",
+      "안산",
+      "안성",
+      "안양",
+      "양주",
+      "양평",
+      "고양",
+      "여주",
+      "연천",
+      "오산",
+      "용인",
+      "의왕",
+      "의정부",
+      "이천",
+      "파주",
+      "평택",
+      "포천",
+      "과천",
+      "하남",
+      "화성",
+      "광명",
+      "광주",
+      "구리",
+      "군포",
+      "김포",
+      "남양주",
+    ],
+    [
+      "강릉",
+      "인제",
+      "정선",
+      "철원",
+      "춘천",
+      "태백",
+      "평창",
+      "홍천",
+      "화천",
+      "횡성",
+      "고성",
+      "동해",
+      "삼척",
+      "속초",
+      "양구",
+      "양양",
+      "영월",
+      "원주",
+    ],
+    [
+      "괴산",
+      "청주",
+      "충주",
+      "증평",
+      "단양",
+      "보은",
+      "영동",
+      "옥천",
+      "음성",
+      "제천",
+      "진천",
+    ],
+    [
+      "공주",
+      "예산",
+      "천안",
+      "청양",
+      "태안",
+      "홍성",
+      "계룡",
+      "금산",
+      "논산",
+      "당진",
+      "보령",
+      "부여",
+      "서산",
+      "서천",
+      "아산",
+    ],
+    [
+      "고창",
+      "임실",
+      "장수",
+      "전주",
+      "정읍",
+      "진안",
+      "군산",
+      "김제",
+      "남원",
+      "무주",
+      "부안",
+      "순창",
+      "완주",
+      "익산",
+    ],
+    [
+      "강진",
+      "보성",
+      "순천",
+      "신안",
+      "여수",
+      "영광",
+      "영암",
+      "완도",
+      "장성",
+      "고흥",
+      "장흥",
+      "진도",
+      "함평",
+      "해남",
+      "화순",
+      "곡성",
+      "광양",
+      "구례",
+      "나주",
+      "담양",
+      "목포",
+      "무안",
+    ],
+    [
+      "경산",
+      "성주",
+      "안동",
+      "안동",
+      "영덕",
+      "영양",
+      "영주",
+      "영천",
+      "예천",
+      "울릉",
+      "울진",
+      "의성",
+      "경주",
+      "청도",
+      "청송",
+      "칠곡",
+      "포항",
+      "고령",
+      "구미",
+      "김천",
+      "문경",
+      "봉화",
+      "상주",
+    ],
+    [
+      "거제",
+      "양산",
+      "의령",
+      "진주",
+      "창녕",
+      "창원",
+      "통영",
+      "하동",
+      "함안",
+      "거창",
+      "함양",
+      "합천",
+      "고성",
+      "김해",
+      "남해",
+      "밀양",
+      "사천",
+      "산청",
+    ],
   ];
   return (
     <>
       <NoMarginTitle>원하는 지역을 선택해 주세요.</NoMarginTitle>
-      <DestinationButton items={items} />
+      <Block>
+        {items.map((item, idx) => {
+          return (
+            <StyledButton
+              onClick={() => {
+                setButtonIdx(idx);
+              }}
+            >
+              {item}
+            </StyledButton>
+          );
+        })}
+      </Block>
+      {buttonIdx >= 9 && (
+        <>
+          <DownOutlined style={{ marginTop: "30px" }} />
+          <Block style={{ marginTop: "30px" }}>
+            {optionalItems[buttonIdx - 9].map((item) => {
+              return <StyledButton>{item}</StyledButton>;
+            })}
+          </Block>
+        </>
+      )}
     </>
   );
 }
